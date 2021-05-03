a1bea65e30a4b1078393a4d5bf10b6dc9a3ef1af
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-03 22:29:13 +0530

    Array List implementation and sorting using Comparator

--- CollectionExample/src/com/praveen/shethe/collection/ArrayListExample.java
+++ CollectionExample/src/com/praveen/shethe/collection/ArrayListExample.java
@@ -1,7 +1,10 @@
 package com.praveen.shethe.collection;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by Praveenkumar on 5/3/2021.
@@ -10,7 +13,52 @@ public class ArrayListExample {
 
     public static void main(String[] args) {
         List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee("Abhi", "Koala" ,"Zakir", 22, 230000));
+        employees.add(new Employee("Mbhi", "Aoala" ,"Zakir", 22, 230231));
+        employees.add(new Employee("Lbhi", "Poala" ,"Wakir", 22, 2305678));
+        employees.add(new Employee("Kbhi", "Ooala" ,"Rakir", 22, 230098));
+        employees.add(new Employee("Jbhi", "Ioala" ,"Qakir", 22, 230009));
+        employees.add(new Employee("Ibhi", "Uoala" ,"Fakir", 22, 230062));
+        employees.add(new Employee("Hbhi", "Yoala" ,"Aakir", 22, 230089));
+        employees.add(new Employee("Gbhi", "Toala" ,"ZXakir", 22, 230001));
+        employees.add(new Employee("Fbhi", "Roala" ,"Vakir", 22, 230087));
+        employees.add(new Employee("Eabhi", "Eoala" ,"Bakir", 22, 230076));
+        employees.add(new Employee("Dbhi", "Foala" ,"Bakir", 22, 230092));
+        employees.add(new Employee("Cbhi", "Doala" ,"Nakir", 22, 230082));
+        employees.add(new Employee("bbhi", "Xoala" ,"Wakir", 22, 230083));
+        employees.add(new Employee("Abhi", "Zoala" ,"Uakir", 22, 230077));
+
+        System.out.println("============================== UNSORTED LIST ======================================");
+        System.out.println(employees);
+        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee employee1, Employee employee2) {
+                return employee1.getName().compareTo(employee2.getName());
+            }
+        });
+        System.out.println("============================== (Name)SORTED LIST ======================================");
+        System.out.println(employees);
+        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee employee1, Employee employee2) {
+                return employee1.getLastName().compareTo(employee2.getLastName());
+            }
+        });
+        System.out.println("============================== (MiddleName)SORTED LIST ======================================");
+        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee employee1, Employee employee2) {
+                return employee1.getMiddleName().compareTo(employee2.getMiddleName());
+            }
+        });
+        System.out.println(employees);
+        System.out.println("============================== (LastName)SORTED LIST ======================================");
+        Collections.sort(employees, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee employee1, Employee employee2) {
+                return employee1.getLastName().compareTo(employee2.getLastName());
+            }
+        });
+        System.out.println(employees);
     }
 }
 
@@ -66,5 +114,29 @@ class Employee {
 
     public void setSalary(int salary) {
         this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        Employee employee = (Employee) o;
+        return age == employee.age && salary == employee.salary && Objects.equals(name, employee.name) && Objects.equals(middleName, employee.middleName) && Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, middleName, lastName, age, salary);
     }
 }
