eb50eaea06a54bbae98cc77d1121ddb7e59857dc
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-03 20:04:19 +0530

    Hello world and ignore file

---
+++ CollectionExample/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Praveenkumar on 5/3/2021.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world, Java");
+    }
+
+}
