951d8adecc69652a99c1f062a93b254af1b4a27d
Praveenkumar <PraveenkumarShethe@users.noreply.github.com>
2021-05-06 02:32:30 +0530

    Immutability test and implementation

--- CollectionExample/src/com/praveen/shethe/simplejava/TestingImmutability.java
+++ CollectionExample/src/com/praveen/shethe/simplejava/TestingImmutability.java
@@ -1,5 +1,8 @@
 package com.praveen.shethe.simplejava;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Praveenkumar on 5/6/2021.
  */
@@ -7,9 +10,21 @@ public class TestingImmutability {
 
     public static void main(String[] args) {
 
+        Map<Person, Integer> map = new HashMap();
+
         PersonImmutable personImmutable = new PersonImmutable("praveenkumar", 37, "3421");
 
         Person person = new Person(personImmutable, 37);
+
+        PersonImmutable personImmutable1 = new PersonImmutable("praveenkumar", 37, "3421");
+
+        Person person1 = new Person(personImmutable, 37);
+
+        map.put(person, 123);
+
+        map.put(person1, 234);
+
+        System.out.println(map);
 
 
     }
